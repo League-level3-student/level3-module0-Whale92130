dd8fc416cb43a243641dc6efa81ab30427a9a771
jereycui2008 <jeremycuid@gmail.com>
2023-05-25 16:55:43 -0700

    worked on 2d arrays

--- src/_03_Intro_to_2D_Arrays/_01_IntroTo2DArrays.java
+++ src/_03_Intro_to_2D_Arrays/_01_IntroTo2DArrays.java
@@ -5,17 +5,24 @@ public class _01_IntroTo2DArrays {
     public static void main(String[] args) {
         // 1. declare a 2D array of integers with 3, 1D arrays that each hold
         //    3 integers
-
+    	int[][] twoDArray = new int[3][3];
         // 2. use two for loops to set all the elements in the 2D array to
         //    a different integer
         //    BE SURE TO USE THE ARRAY'S length VARIABLE
+    	int count = 1;
+    	for (int i =0; i<twoDArray.length; i++) {
+    		for (int o = 0; o<twoDArray.length; o++) {
+    			twoDArray[i][o] = count;
+    			count++;
+    		}
+    	}
         
         // 3. print the third element from the second 1D array (2DArray[1][2])
-
+    	System.out.println(twoDArray[1][2]);
         // 4. set the third element from the second 1D array to a different
         //    value
-
+    	twoDArray[1][2] = 21;
         // 5. print the element again and verify the value has changed
-
+    	System.out.println(twoDArray[1][2]);
     }
 }
--- src/_03_Intro_to_2D_Arrays/_02_2DArrayPractice.java
+++ src/_03_Intro_to_2D_Arrays/_02_2DArrayPractice.java
@@ -24,28 +24,51 @@ package _03_Intro_to_2D_Arrays;
  *  Column 0 = matrix[0][0], matrix[1][0], matrix[2][0] // 1, 4, 7
  */
 public class _02_2DArrayPractice {
-    /*
-     * Return a 2D array that can accommodate the grid of values below:
-     *          0, 1, 2
-     *          3, 4, 5
-     *          6, 7, 8
-     */
-    public Integer[][] test1() {
-        
-        return null;
-    }
-    
-    /*
-     * Return a 2D array that can accommodate the grid of values below:
-     *          "a", "b", "c", "d",
-     *          "e", "f", "g", "e"
-     */
-    public String[][] test2() {
-        
-        return null;
-    }
-    
-    /*
+	
+	/*
+	 * Return a 2D array that can accommodate the grid of values below: 0, 1, 2 3,
+	 * 4, 5 6, 7, 8
+	 */
+	public Integer[][] test1() {
+		Integer[][] test1 = new Integer[3][3];
+		int count = 0;
+		for (int i = 0; i < test1.length; i++) {
+			for (int o = 0; o < test1.length; o++) {
+				test1[i][o] = count;
+				count++;
+			}
+		}
+		return test1;
+	}
+
+	/*
+	 * Return a 2D array that can accommodate the grid of values below: 
+	 * "a", "b", c d
+	 *  "e", "f", "g", "e"
+	 */
+	public String[][] test2() {
+		String[][] test2 = new String[2][4];
+		String[] letters = new String[8];
+		letters[0] = "a";
+		letters[1] = "b";
+		letters[2] = "c";
+		letters[3] = "d";
+		letters[4] = "e";
+		letters[5] = "f";
+		letters[6] = "g";
+		letters[7] = "e";
+		int count = 0;
+		for (int i = 0; i < test2.length; i++) {
+			for (int o = 0; o < test2[test2.length-1].length; o++) {
+				test2[i][o] = letters[count];
+				System.out.println(test2[i][o]);
+				count++;
+			}
+		}
+		return test2;
+	}
+
+	/*
      * Return a 2D array that can accommodate the grid of values below:
      *          true, false,
      *          false, false,
@@ -54,8 +77,20 @@ public class _02_2DArrayPractice {
      *          true, false
      */
     public Boolean[][] test3() {
-        
-        return null;
+        Boolean[][] test3 = new Boolean[5][2];
+        int count = 0;
+        for (int i = 0; i < test3.length; i++) {
+			for (int o = 0; o < test3[test3.length-1].length; o++) {
+				if(count == 0 || count == 7 || count == 8 || count == 9) {
+					test3[i][o] = true;
+				}
+				else {
+					test3[i][o] = false;
+				}
+				count++;
+				}
+			}
+        return test3;
     }
     
     /*
@@ -64,7 +99,8 @@ public class _02_2DArrayPractice {
      * 
      * int[][] grid = new int[2][3];
      * 
-     * 
+     *  2, 4, 9
+     *  3, 7, 10
      * 
      * Show your teacher the grid you created
      */
@@ -75,7 +111,10 @@ public class _02_2DArrayPractice {
      * 
      * Robot[][] robots = new Robot[4][2];
      * 
-     * 
+     * robots[0], robots[1]
+     * robots[2], robots[3]
+     * robots[4], robots[5]
+     * robots[6], robots[7]
      * 
      * Show your teacher the grid you created
      */
@@ -86,8 +125,15 @@ public class _02_2DArrayPractice {
      */
     public Object test4() {
         Object[][] objects = { {null, null, null, null}, {null, null, null, new Object()} };
-        
-        return null;
+        Object notNull =null;
+        for (int i = 0; i < objects.length; i++) {
+			for (int o = 0; o < objects[objects.length-1].length; o++) {
+				if(objects[i][o] != null) {
+					notNull = objects[i][o];
+				}
+				}
+			}
+        return notNull;
     }
     
     /* Given the array of Objects below, return the element that is not null
@@ -96,8 +142,15 @@ public class _02_2DArrayPractice {
      */
     public Object test5() {
         Object[][] objects = { {null, null, new Object(), null}, {null, null, null, null} };
-        
-        return null;
+        Object notNull =null;
+        for (int i = 0; i < objects.length; i++) {
+			for (int o = 0; o < objects[objects.length-1].length; o++) {
+				if(objects[i][o] != null) {
+					notNull = objects[i][o];
+				}
+				}
+			}
+        return notNull;
     }
     
     /*
@@ -106,8 +159,11 @@ public class _02_2DArrayPractice {
      */
     public Integer test6() {
         int[][] nums = { {8, 7, 6, 5}, {4, 3}, {2, 1, 0} };
-        
-        return null;
+        int total = 0;
+        for (int i = 0; i < nums[0].length; i++) {
+			total = total + nums[0][i];
+			}
+        return total;
     }
     
     /*
@@ -116,8 +172,11 @@ public class _02_2DArrayPractice {
      */
     public Integer test7() {
         int[][] nums = { {8, 7, 6, 5}, {4, 3}, {2, 1, 0} };
-        
-        return null;
+        int total = 0;
+        for (int i = 0; i < nums.length; i++) {
+			total = total + nums[i][0];
+			}
+        return total;
     }
     
     /*
@@ -125,8 +184,11 @@ public class _02_2DArrayPractice {
      * specified row
      */
     public Integer getSumByRow(int[][] matrix, int row) {
-        
-        return null;
+    	int total = 0;
+        for (int i = 0; i < matrix[row].length; i++) {
+			total = total + matrix[row][i];
+			}
+        return total;
     }
     
     /*
@@ -134,8 +196,11 @@ public class _02_2DArrayPractice {
      * specified column
      */
     public Integer getSumByColumn(int[][] matrix, int col) {
-        
-        return null;
+    	int total = 0;
+        for (int i = 0; i < matrix.length; i++) {
+			total = total + matrix[i][col];
+			}
+        return total;
     }
     
     /*
@@ -158,8 +223,14 @@ public class _02_2DArrayPractice {
      *    boundaries, return null.
      */
     public Integer getEastNeighbor(int[][] matrix, int row, int col) {
-        
-        return null;
+    	//start here
+    	//
+    	//
+    	//
+    	int total = 0;
+    	total = matrix[row-1][col];
+
+        return total;
     }
     
     public Integer getWestNeighbor(int[][] matrix, int row, int col) {
