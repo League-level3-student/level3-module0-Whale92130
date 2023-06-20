8690a5777d197e14437f0ffdbea655ca58c67468
league <league@imac-14.attlocal.net>
2023-06-20 11:55:56 -0700

    finfhsed moduale

--- src/_07_The_Wrong_Way_Cow/TheWrongWayCow.java
+++ src/_07_The_Wrong_Way_Cow/TheWrongWayCow.java
@@ -50,30 +50,92 @@ package _07_The_Wrong_Way_Cow;
 
 public class TheWrongWayCow {
 
-    public static int[] findWrongWayCow(final char[][] field) {
-        // Fill in the code to return the [col, row] coordinate position of the
-        // head (letter 'c') of the wrong way cow!
-    	
-    	int[] total = new int[2];
-        for (int i = 0; i <field.length; i++) {
-        	for (int o= 0; o <field[0].length; o++) {
-        		if(field[i][o] == 'c') {
-        			total[1] = i;
-        			
-        			total[0] = o;
-        			
-        			System.out.println(total[0] + " " + total[1]);
-       
-        			  
-        		}
-        		else {
-        			System.err.println("fail");
-        			
-        		}
-        		
-        	}
-        }
+	public static int[] findWrongWayCow(final char[][] field) {
+		// Fill in the code to return the [col, row] coordinate position of the
+		// head (letter 'c') of the wrong way cow!
+		int cowHPlus = 0;
+		int cowVPlus = 0;
+		int cowHMinus = 0;
+		int cowVMinus = 0;
+		int[] total1 = new int[2];
+		int[] total2 = new int[2];
+		int[] total3 = new int[2];
+		int[] total4 = new int[2];
+		int[] total = new int[2];
+		for (int i = 0; i < field.length; i++) {
+			for (int o = 0; o < field[0].length; o++) {
+
+				// check vertical +
+				if (i + 2 <= field.length-1) {
+					if (field[i][o] == 'c' && field[i + 1][o] == 'o' && field[i + 2][o] == 'w') {
+						total1[1] = i;
+						total1[0] = o;
+						
+						cowVPlus++;
+					}
+				}
+				// check horizontal +
+				if (o + 2 <= field[0].length) {
+
+					if (field[i][o] == 'c' && field[i][o + 1] == 'o' && field[i][o + 2] == 'w') {
+						total2[1] = i;
+						total2[0] = o;
+						
+						cowHPlus++;
+					}
+				}
+
+				// check vertical -
+				if (i - 2 >= 0) {
+					if (field[i][o] == 'c' && field[i - 1][o] == 'o' && field[i - 2][o] == 'w') {
+						total3[1] = i;
+						total3[0] = o;
+						
+						cowVMinus++;
+					}
+				}
+
+				// check horizontal -
+				if (o - 2 >= 0) {
+				
+					if (field[i][o] == 'c' && field[i][o - 1] == 'o' && field[i][o - 2] == 'w') {
+						total4[1] = i;
+						total4[0] = o;
+						
+						cowHMinus++;
+					}
+				}
+			
+				
+				
+				
+				
+			}
+		
+		}
+		// do logic
+		System.out.println("cowVPlus = " + cowVPlus);
+		System.out.println("cowVMinus = " + cowVMinus);
+		System.out.println("cowHPlus = " + cowHPlus);
+		System.out.println("cowHMinus = " + cowHMinus);
+		if (cowVPlus == 1) {
+			total = total1;
+			
+		} else if (cowVMinus == 1) {
+			total = total3;
+			
+		} else if (cowHPlus == 1) {
+			total = total2;
+
+		} else if (cowHMinus == 1) {
+			total = total4;
+
+		}
+		cowHPlus = 0;
+		 cowVPlus = 0;
+		 cowHMinus = 0;
+		 cowVMinus = 0;
 		return total;
-     
-    }
+
+	}
 }
